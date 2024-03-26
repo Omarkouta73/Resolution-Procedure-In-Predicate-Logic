b1358c0e275f895da0d6f24a1a19d30cff2ceead
OmarKouta73 <omarkouta551@gmail.com>
2024-03-26 17:02:17 +0200

    first three functions done

--- main.py
+++ main.py
@@ -1,11 +1,101 @@
-def eliminate_implication():
-    pass
+def remove_biconditional(expression):
+    # A <--> B == ["iff", "A", "B"]
+    if expression[0] == "iff":
+        literal1 = expression[1]
+        literal2 = expression[2]
+        expression[0] = "and"
+        expression[1] = ["implies", literal1, literal2]
+        expression[2] = ["implies", literal2, literal1]
+
+    for literal in expression:
+        if len(literal) > 1:
+            remove_biconditional(literal)
+
+def remove_imply(expression):
+    # A --> B [implies, A, B] == [or, [not, A], B]
+    if expression[0] == "implies":
+        literal1 = expression[1]
+
+        expression[0] = "or"
+        expression[1] = ["not", literal1]
+
+    for literal in expression:
+        if len(literal) > 1:
+            remove_imply(literal)
 
 
-def move_negation_inward():
+def eliminate_implication(expression):
+    remove_biconditional(expression)
+    remove_imply(expression)
+
+
+def deMorgan(expression): # [not, [not, P]] == [and, [not, literal1], [not, literal2]]
+    if expression[0] == "not":
+        literal = expression[1]
+
+
+        if literal[0] == "not":
+            del expression[:]
+            # check if variable, then append
+            if (len(literal[1]) == 1):
+                expression.append(literal[1])
+            # if operator or list
+            else:
+                for lit in literal[1]:
+                    expression.append(lit)
+
+                # recursive on inner lists
+                if (len(expression) > 1):
+                    deMorgan(expression)
+
+
+        elif literal[0] == "or":
+            del expression[:]
+            expression.append("and")
+            for lit in literal:
+                if lit == "or":
+                    continue
+                else:
+                    expression.append(["not", lit])
+
+
+        elif literal[0] == "and":
+            del expression[:]
+            expression.append("or")
+            for lit in literal:
+                if lit == "and":
+                    continue
+                else:
+                    expression.append(["not", lit])
+
+
+        elif literal[0] == "exist": # [not, [exist, P]] == [all, [not, P]]
+            del expression[:]
+            expression.append("all")
+            for lit in literal:
+                if lit == "exist":
+                    continue
+                else:
+                    expression.append(["not", lit])
+
+
+        elif literal[0] == "all": # [not, [all, P]] == [exist, [not, P]]
+            del expression[:]
+            expression.append("exist")
+            for lit in literal:
+                if lit == "all":
+                    continue
+                else:
+                    expression.append(["not", lit])
+
+    for literal in expression:
+        if len(literal) > 1:
+            deMorgan(literal)
+
+
+def move_negation_inward(expression):
     # De Morgan Law
-    # remove double nots
-    pass
+    deMorgan(expression)
 
 
 def standardize_variable_names():
@@ -42,10 +132,10 @@ def unification():
     pass
 
 
-def convert_to_cnf():
+def convert_to_cnf(expression):
     # IDEA : return a set or a list of clauses
-    eliminate_implication()
-    move_negation_inward()
+    eliminate_implication(expression)
+    move_negation_inward(expression)
     standardize_variable_names()
     move_quantifiers_left()
 
@@ -66,3 +156,6 @@ def apply_resolution():
 
     # resolution
     pass
+
+
+print(deMorgan(["all", ["not", ["exist", "A"]]]))
